c051be575d5af0a50c65289c951cb933b6090a63
unknown <adamhajdik@seznam.cz>
2021-05-01 00:13:15 +0200

    -Rozšíření historické stránky

--- js/historie.js
+++ js/historie.js
@@ -5,9 +5,8 @@ const heroes = [
        "death":"17.12.1830",
        "biography":"Simón Bolívar byl synem šlechtice španělského původu; otec mu zemřel ve věku tří let, matka, když mu bylo dvanáct. Ujal se ho strýc, se kterým se ale Simón pohádal a tři roky po smrti své matky se odstěhoval ke svému učiteli Simónu Rodríguezovi. Roku 1798 vstoupil Bolívar do venezuelské milice jako kadet. Roku 1799 odešel studovat do Španělska. Pokračoval do Francie, kde studoval francouzštinu, vedl rozmařilý život a stal se velkým přívržencem Napoleona Bonaparte. Roku 1802 se vrátil do Španělska, kde se oženil se svou příbuznou Maríí Teresou Rodríguezovou del Toro. Nakrátko se vrátil do vlasti v letech 1803–1804, nedlouho po příjezdu jeho manželka María Teresa zemřela. 2. prosince 1804 se Bolívar zúčastnil Napoleonovy korunovace. O rok později byl přizván mezi Svobodné zednáře. Do vlasti se definitivně vrátil roku 1807. Zapojil se do hnutí usilujícího o osvobození ze španělské nadvlády. ",
        "portraits":[
-          "",
-          "",
-          ""
+          "bolivar.jpg",
+          "bolivar2.jpg"
        ],
        "online":"https://cs.wikipedia.org/wiki/Sim%C3%B3n_Bol%C3%ADvar"
     },
@@ -17,22 +16,19 @@ const heroes = [
         "death":"20.4.1947",
         "biography":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id eleifend mi. Suspendisse potenti. Donec vehicula turpis justo, accumsan vestibulum mauris pharetra id. Aliquam tellus neque, aliquam vitae posuere non, volutpat id metus. Sed rhoncus lorem vel dui faucibus sodales. Maecenas dictum augue sit amet nisi suscipit, ac vulputate sapien dignissim. Fusce malesuada, urna ut facilisis placerat, dui odio maximus.",
         "portraits":[
-           "",
-           "",
-           "",
-           ""
+           "patino.jpg",
+           "patino2.jpg"
         ],
         "online":"https://cs.wikipedia.org/wiki/Marco_Etcheverry"
      },
      {
-        "name":"Marco Etcheverry",
-        "birth":"26.9.1970",
-        "death":"-",
+        "name":"Francisco Pizarro",
+        "birth":"16.3.1478",
+        "death":"26.3.1541",
         "biography":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id eleifend mi. Suspendisse potenti. Donec vehicula turpis justo, accumsan vestibulum mauris pharetra id. Aliquam tellus neque, aliquam vitae posuere non, volutpat id metus. Sed rhoncus lorem vel dui faucibus sodales. Maecenas dictum augue sit amet nisi suscipit, ac vulputate sapien dignissim. Fusce malesuada, urna ut facilisis placerat, dui odio maximus.",
         "portraits":[
-           "",
-           "",
-           ""
+           "pizarro.png",
+           "pizarro2.jpg"
         ],
         "online":"https://cs.wikipedia.org/wiki/V%C3%A1clav_Havel"
      },
@@ -42,8 +38,8 @@ const heroes = [
         "death":"15.11.1670",
         "biography":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id eleifend mi. Suspendisse potenti. Donec vehicula turpis justo, accumsan vestibulum mauris pharetra id. Aliquam tellus neque, aliquam vitae posuere non, volutpat id metus. Sed rhoncus lorem vel dui faucibus sodales. Maecenas dictum augue sit amet nisi suscipit, ac vulputate sapien dignissim. Fusce malesuada, urna ut facilisis placerat, dui odio maximus.",
         "portraits":[
-           "",
-           "",
+           "gomez.jpg",
+           "gomez2.jpg",
         ],
         "online":"https://en.wikipedia.org/wiki/Roberto_Su%C3%A1rez_G%C3%B3mez"
      },
@@ -53,9 +49,8 @@ const heroes = [
          "death":"6.7.1415",
          "biography":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id eleifend mi. Suspendisse potenti. Donec vehicula turpis justo, accumsan vestibulum mauris pharetra id. Aliquam tellus neque, aliquam vitae posuere non, volutpat id metus. Sed rhoncus lorem vel dui faucibus sodales. Maecenas dictum augue sit amet nisi suscipit, ac vulputate sapien dignissim. Fusce malesuada, urna ut facilisis placerat, dui odio maximus.",
          "portraits":[
-            "",
-            "",
-            ""
+            "juana.jpg",
+            "juana2.jpg"
          ],
          "online":"https://en.wikipedia.org/wiki/Juana_Azurduy_de_Padilla"
       },
@@ -65,8 +60,8 @@ const heroes = [
          "death":"1.5.1904",
          "biography":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id eleifend mi. Suspendisse potenti. Donec vehicula turpis justo, accumsan vestibulum mauris pharetra id. Aliquam tellus neque, aliquam vitae posuere non, volutpat id metus. Sed rhoncus lorem vel dui faucibus sodales. Maecenas dictum augue sit amet nisi suscipit, ac vulputate sapien dignissim. Fusce malesuada, urna ut facilisis placerat, dui odio maximus.",
          "portraits":[
-            "",
-            "",
+            "jaime.jpg",
+            "jaime2.jpg",
          ],
          "online":"https://en.wikipedia.org/wiki/Jaime_Escalante"
       },
@@ -76,7 +71,8 @@ const heroes = [
         "death":"28.1.1930",
         "biography":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id eleifend mi. Suspendisse potenti. Donec vehicula turpis justo, accumsan vestibulum mauris pharetra id. Aliquam tellus neque, aliquam vitae posuere non, volutpat id metus. Sed rhoncus lorem vel dui faucibus sodales. Maecenas dictum augue sit amet nisi suscipit, ac vulputate sapien dignissim. Fusce malesuada, urna ut facilisis placerat, dui odio maximus.",
         "portraits":[
-           "",
+           "lidia.jpg",
+           "lidia2.png"
         ],
         "online":"https://en.wikipedia.org/wiki/Lidia_Gueiler_Tejada"
      },
@@ -85,7 +81,6 @@ const heroes = [
 $(function(){
 
    $("h2").on("click", function(){
-        
         $(this).parents(".row").next().toggle(500);
       });   
             
@@ -105,7 +100,7 @@ $(function(){
         $("#portret .card-footer").html(`<a href="${hero.online}">Odkaz: ${hero.online}</a>`);
         $("#portret .gallery").empty();
         hero.portraits.forEach(portrait => {
-           $("#portret .gallery").append(`<div class="col-sm-4"><img src="images/${portrait}" alt="${hero.name}" class="img-fluid"></div>`);
+           $("#portret .gallery").append(`<div class="col-sm-4"><img src="../img/h_img/${portrait}" alt="${hero.name}" class="img-fluid"></div>`);
         })
       }
     $("#postavy li:first").addClass("active");
