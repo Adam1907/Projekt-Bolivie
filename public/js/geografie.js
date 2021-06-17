a8a310306928260bc8841f88e5af4fa9d0240f10
unknown <adamhajdik@seznam.cz>
2021-06-17 17:04:38 +0200

    Pridani casu + popisek u obrazku

--- public/js/geografie.js
+++ public/js/geografie.js
@@ -171,3 +171,25 @@ $(function(){
         $("#card-main").slideDown(500);
     });  
 })
+
+function realtimeClock(){
+
+   var rtClock = new Date();
+
+   var hours =   rtClock.getHours();
+   var minutes = rtClock.getMinutes();
+   var seconds = rtClock.getSeconds();
+
+   hours = (hours < 6) ? hours + 18 : hours -6;
+
+   hours = ("0" + hours).slice(-2);
+   minutes = ("0" + minutes).slice(-2);
+   seconds = ("0" + seconds).slice(-2);
+
+   document.getElementById("clock").innerHTML = 
+   hours + " : " + minutes + " : " + seconds;
+
+   var t = setTimeout(realtimeClock, 500);
+}
+
+realtimeClock();
--- public/js/historie.js
+++ public/js/historie.js
@@ -80,4 +80,34 @@ $(function () {
       console.error('Chyba: \n', error);
    });
 
+   function realtimeClock(){
+
+      var rtClock = new Date();
+   
+      var hours =   rtClock.getHours();
+      var minutes = rtClock.getMinutes();
+      var seconds = rtClock.getSeconds();
+   
+      hours = (hours < 6) ? hours + 18 : hours -6;
+   
+      hours = ("0" + hours).slice(-2);
+      minutes = ("0" + minutes).slice(-2);
+      seconds = ("0" + seconds).slice(-2);
+   
+      document.getElementById("clock").innerHTML = 
+      hours + " : " + minutes + " : " + seconds;
+   
+      var t = setTimeout(realtimeClock, 500);
+   }
+   
+   realtimeClock();
+
+
+   $(".imgP").on('click', function(){
+      $(this).popover({
+         "trigger": "click",
+         "placement": "right", 
+      })
+         })
+
 })
